721ea3d85fcf0f523c9d0709d22dc42a08c085e5
pogopaule <pogopaule@gmx.de>
2015-09-05 19:59:28 +0200

    add scrape script

---
+++ scrape.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+date=$(date +"%d-%m-%y")
+mkdir $date
+
+for hour in {8..21}
+do
+  paddedHour=$(printf "%02d" $hour)
+  for minute in 00 15 30 45
+  do
+    wget http://www.dgfc-suedschwarzwald.de/webcam2/image-${paddedHour}-${minute}.jpg -O $date/$paddedHour-$minute
+  done
+done
+
+
