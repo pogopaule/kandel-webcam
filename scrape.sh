2abbb1576b402e25304b6b436f8fd8936821e4fc
pogopaule <pogopaule@gmx.de>
2015-09-21 20:27:53 +0200

    save with img extension

--- scrape.sh
+++ scrape.sh
@@ -8,7 +8,7 @@ do
   paddedHour=$(printf "%02d" $hour)
   for minute in 00 15 30 45
   do
-    wget http://www.dgfc-suedschwarzwald.de/webcam2/image-${paddedHour}-${minute}.jpg -O $date/$paddedHour-$minute
+    wget http://www.dgfc-suedschwarzwald.de/webcam2/image-${paddedHour}-${minute}.jpg -O $date/$paddedHour-${minute}.jpg
   done
 done
 
